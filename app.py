368419b03c3e84d868300490688058c42dbb5d86
Meghna <chaudharymeghna07@gmail.com>
2025-11-17 01:41:27 +0530

    initial push

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask,  render_template
 
 app= Flask(__name__)
 
 # defining routes
 
 @app.route("/")
-def index:
-    print("Home page")
+def home():
+     return render_template("index.html", active="home")
+ 
+@app.route("/login")
+def login():
+    return render_template("login.html", active="login")
+
+@app.route("/register")
+def register():
+    return render_template("register.html", active="register")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin_dashboard.html")
+
+@app.route("/patient")
+def patient():
+     return render_template("patient_dashboard.html")
+
+@app.route("/doctor")
+def doctor():
+     return render_template("doctor_dashboard.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
